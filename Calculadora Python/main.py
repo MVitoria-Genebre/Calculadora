846e28d7b1517b454b63937a4ff82e486d9bff8c
MVitoria-Genebre <freiresvitoria2@gmail.com>
2025-05-05 21:41:11 -0300

    Derivadas

--- Calculadora Python/main.py
+++ Calculadora Python/main.py
@@ -1,2 +1,12 @@
-from tkinter import *
-from tkinter import ttk
+import sympy as sy
+from sympy.interactive import init_printing
+init_printing(pretty_print=True)
+
+#-----------DERIVADA DA FUNÇÃO--------------
+#DECLARANDO A VARIAVEL SIMBOLICA
+x = sy.Symbol('x')
+#EXPLICITANDO A FUNÇÃO
+funcao = x**10
+#RESOLVENDO A FUNÇÃO
+resultado = sy.diff(funcao)
+resultado
